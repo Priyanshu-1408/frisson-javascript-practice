e5107200a145c71e165beb0ed204eb7542205f9a
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-10 12:24:21 +0530

    fix the comments of value_exist_or_not

--- value_exist_or_not.js
+++ value_exist_or_not.js
@@ -1,12 +1,33 @@
-let arr = [2,4,7,9,2];
+// let arr = [2,4,7,9,2];
+
+// let no = 4;
+
+// let ans = arr.indexOf(no);
+// // console.log(ans);
+
+// if(ans == -1){
+//     console.log("no exist");
+// }else{
+//     console.log("exist");
+// }
+
+
+
+let arr = [2, 4, 7, 9, 2];
 
 let no = 4;
 
-let ans = arr.indexOf(no);
-// console.log(ans);
+let found = false;
 
-if(ans == -1){
-    console.log("no exist");
-}else{
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === no) {
+        found = true;
+        break;
+    }
+}
+
+if (found) {
     console.log("exist");
+} else {
+    console.log("no exist");
 }
