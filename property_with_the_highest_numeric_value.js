af903ab1a51575ead5e96d49f32206e5269797dd
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-10 12:42:24 +0530

    fix the comments in property_with_the_highest_numeric_value

--- property_with_the_highest_numeric_value.js
+++ property_with_the_highest_numeric_value.js
@@ -1,8 +1,25 @@
+// const saman = {
+//     rice : 10,
+//     daal : 20,
+//     sabzi : 40
+// }
+
+// const maxVal = Math.max(...Object.values(saman));
+// console.log(maxVal);
+
+
 const saman = {
-    rice : 10,
-    daal : 20,
-    sabzi : 40
+    rice: 10,
+    daal: 20,
+    sabzi: 40
+};
+
+let maxVal = -Infinity;
+
+for (let key in saman) {
+    if (saman[key] > maxVal) {
+        maxVal = saman[key];
+    }
 }
 
-const maxVal = Math.max(...Object.values(saman));
 console.log(maxVal);
