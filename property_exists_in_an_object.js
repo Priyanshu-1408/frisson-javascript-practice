bc54f99595b6971c12bb695a57ebd94a33a66517
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-10 15:33:48 +0530

    remove the hasown function of object and do this question through for loop

--- property_exists_in_an_object.js
+++ property_exists_in_an_object.js
@@ -1,7 +1,33 @@
+// // let students = {
+// //     name:"priyanshu",
+// //     class : "11th",
+// //     schol : "sps"
+// // }
+
+// // console.log(Object.hasOwn(students,"nae"));
+
+
+
+
 let students = {
-    name:"priyanshu",
-    class : "11th",
-    schol : "sps"
+    name: "priyanshu",
+    class: "11th",
+    schol: "sps"
+};
+
+let find = "name";
+
+let found = false;
+
+for (let key in students) {
+    if (key === find) {
+        found = true;
+        break;
+    }
 }
 
-console.log(Object.hasOwn(students,"nae"));
+if (found) {
+    console.log("exist");
+} else {
+    console.log("not exist");
+}
