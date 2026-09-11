f9d9c94a8c028fef8d722f405824067425e2522e
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-11 10:45:54 +0530

    remove .age so that whole object is swapped

--- Sort_an_array_of_objects_by_age.js
+++ Sort_an_array_of_objects_by_age.js
@@ -8,11 +8,18 @@ const people = [
 for(let i=0;i<people.length;i++){
     for(let j=i+1;j<people.length;j++){
         if(people[i].age > people[j].age){
-            let temp = people[i].age;
-            people[i].age = people[j].age;
-            people[j].age = temp;
+            let temp = people[i];
+            people[i] = people[j];
+            people[j] = temp;
         }
     }
 }
 
 console.log(people);
+
+
+[
+    { name: "B", age: 20 },
+    { name: "A", age: 25 },
+    { name: "C", age: 30 }
+]
