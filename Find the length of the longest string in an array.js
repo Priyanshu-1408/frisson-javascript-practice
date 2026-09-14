1a15a5bf1d82a0a25ff55d6ace6ed5e11f797616
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-14 12:59:45 +0530

    Find the length of the longest string in an array.

---
+++ Find the length of the longest string in an array.js
@@ -0,0 +1,12 @@
+let arr = ["apple", "banana", "cat", "elephant"];
+
+let maxLength = 0;
+
+for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i].length > maxLength) {
+        maxLength = arr[i].length;
+    }
+}
+
+console.log(maxLength);
