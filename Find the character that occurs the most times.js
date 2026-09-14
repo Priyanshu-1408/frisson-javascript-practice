51264fdf1d9137eeef33e1ddb9da8279f14ccbd3
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-14 10:46:48 +0530

    Find the character that occurs the most times

---
+++ Find the character that occurs the most times.js
@@ -0,0 +1,23 @@
+let str = "hello world";
+
+let maxChar = "";
+let maxCount = 0;
+
+for (let i = 0; i < str.length; i++) {
+
+    let count = 0;
+
+    for (let j = 0; j < str.length; j++) {
+
+        if (str[i] === str[j]) {
+            count++;
+        }
+    }
+
+    if (count > maxCount) {
+        maxCount = count;
+        maxChar = str[i];
+    }
+}
+
+console.log(maxChar); 
