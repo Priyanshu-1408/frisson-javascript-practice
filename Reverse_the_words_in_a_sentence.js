b57b061537b36fed0b64d4b23d3cbd9c976b2b50
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-14 10:53:45 +0530

    Reverse_the_words_in_a_sentence

--- Reverse_the_words_in_a_sentence.js
+++ Reverse_the_words_in_a_sentence.js
@@ -1,16 +1,21 @@
 // Reverse the words in a sentence.
 
+import { myPush } from "./push.js";
+import { MySplit } from "./split.js";
+
 // Input: "I love JavaScript"
 // Output: "JavaScript love I"
 
 
 let str = "I love JavaScript";
 
-let words = str.split(" ");
+// let words = str.split(" ");
+let words = MySplit(str , " ");
 
 let arr = [];
 for(let i=words.length-1;i>=0;i--){
-    arr.push(words[i]);
+    // arr.push(words[i]);
+    myPush(arr,words[i]);
 }
 
 console.log(arr);
