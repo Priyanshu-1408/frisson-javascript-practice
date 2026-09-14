17ab5ee2608ecfea41670b78f9541d6742dd0ba1
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-14 11:18:26 +0530

    Find the majority element

---
+++ Find the majority element.js
@@ -0,0 +1,22 @@
+let arr = [2, 2, 1, 1, 1, 2, 2];
+
+let majority = -1;
+
+for (let i = 0; i < arr.length; i++) {
+
+    let count = 0;
+
+    for (let j = 0; j < arr.length; j++) {
+
+        if (arr[i] === arr[j]) {
+            count++;
+        }
+    }
+
+    if (count > arr.length / 2) {
+        majority = arr[i];
+        break;
+    }
+}
+
+console.log(majority);
