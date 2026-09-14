eb672b148c1d77a81061003c59839e414c41c517
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-14 12:33:37 +0530

    Remove duplicate values from an array.

---
+++ Remove duplicate values from an array..js
@@ -0,0 +1,22 @@
+let arr = [1, 2, 2, 3, 4, 3, 5];
+
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+
+    let alreadyExists = false;
+
+    for (let j = 0; j < result.length; j++) {
+
+        if (arr[i] === result[j]) {
+            alreadyExists = true;
+            
+        }
+    }
+
+    if (alreadyExists === false) {
+        result.push(arr[i]);
+    }
+}
+
+console.log(result);
