f046e916cf3a9415e5471e2563def8df293f789b
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-22 14:08:36 +0530

    optimise

--- convert_array_string.js
+++ convert_array_string.js
@@ -1,17 +1,36 @@
-import { myMap } from "./map.js";
+// import { myMap } from "./map.js";
+// import { myUpperCase } from "./uppercase.js";
+
+// let fal = ["apple" , "banana" , "grapses"];
+
+// // const newFal = fal.map(s => s.toUpperCase());
+// let newFal = myMap(fal,function(s){
+//     // return s.toUpperCase();
+//     return myUpperCase(s);
+// })
+
+// for(let i=0;i<newFal.length;i++){
+//     console.log(newFal[i]);
+// }
+
+
+// // ello world
+
+
+
 import { myUpperCase } from "./uppercase.js";
+import { myPush } from "./push.js";
 
-let fal = ["apple" , "banana" , "grapses"];
+let fal = ["apple", "banana", "grapses"];
 
-// const newFal = fal.map(s => s.toUpperCase());
-let newFal = myMap(fal,function(s){
-    // return s.toUpperCase();
-    return myUpperCase(s);
-})
+let newFal = [];
 
-for(let i=0;i<newFal.length;i++){
+for (let i = 0; i < fal.length; i++) {
+    // newFal.push(myUpperCase(fal[i]));
+    myPush(newFal,myUpperCase(fal[i]));
+}
+
+for (let i = 0; i < newFal.length; i++) {
     console.log(newFal[i]);
 }
 
-
-// ello world
