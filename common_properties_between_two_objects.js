060a45bb763e47d16952cdc6a9c9d329fce7fc69
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-22 13:12:50 +0530

    common_properties_between_two_objects

--- common_properties_between_two_objects.js
+++ common_properties_between_two_objects.js
@@ -1,28 +1,52 @@
+// const obj1 = {
+//   name: "A",
+//   age: 20,
+//   city: "Delhi"
+// };
+
+// const obj2 = {
+//   name: "B",
+//   age: 25,
+//   country: "India"
+// };
+
+
+// let result = []; 
+// let keys = Object.keys(obj1);
+
+// console.log(keys);
+
+// for(let i=0;i<keys.length;i++){
+//   if(obj2.hasOwnProperty(keys[i])) {
+//       result.push(keys[i]);
+//     }
+// }
+
+// console.log(result);
+
+
+// // isme abhi thoda confusion hai  
+
+
 const obj1 = {
-  name: "A",
-  age: 20,
-  city: "Delhi"
+    name: "A",
+    age: 20,
+    city: "Delhi"
 };
 
 const obj2 = {
-  name: "B",
-  age: 25,
-  country: "India"
+    name: "B",
+    age: 25,
+    country: "India"
 };
 
+let result = [];
 
-let result = []; 
-let keys = Object.keys(obj1);
+for (let key in obj1) {
 
-console.log(keys);
-
-for(let i=0;i<keys.length;i++){
-  if(obj2.hasOwnProperty(keys[i])) {
-      result.push(keys[i]);
+    if (key in obj2) {
+        result[result.length] = key;
     }
 }
 
 console.log(result);
-
-
-// isme abhi thoda confusion hai  
