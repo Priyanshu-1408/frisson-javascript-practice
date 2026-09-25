1c9e6344db6457315a99c910138317173e15e987
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-25 12:20:39 +0530

    includes

--- includes.js
+++ includes.js
@@ -1,24 +1,59 @@
 export function MyIncludes(str, search) {
 
-    for (let i = 0; i < str.length; i++) {
+        if (search.length === 0) {
+        return true;
+    }
 
-        let match = true;
+    for (let i = 0; i <= str.length - search.length; i++) {
+
+        let isMatch = true;
 
         for (let j = 0; j < search.length; j++) {
 
             if (str[i + j] !== search[j]) {
-                match = false;
+                isMatch = false;
                 break;
             }
         }
 
-        if (match) {
+        if (isMatch) {
             return true;
         }
     }
 
     return false;
 }
+
+console.log(MyIncludes("hello world", "world"));
+                    
+
+
+
+
+
+
+// export function MyIncludes(str, search) {
+
+
+//     for (let i = 0; i < str.length; i++) {
+
+//         let match = true;
+
+//         for (let j = 0; j < search.length; j++) {
+
+//             if (str[i + j] !== search[j]) {
+//                 match = false;
+//                 break;
+//             }
+//         }
+
+//         if (match) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
 
 
 
