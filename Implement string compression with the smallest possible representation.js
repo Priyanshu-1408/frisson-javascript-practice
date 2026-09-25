c9416a2f5cf60c8e31c32357d4c793c56f54e88d
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-25 16:07:47 +0530

    Implement string compression with the smallest possible representation

--- Implement string compression with the smallest possible representation.js
+++ Implement string compression with the smallest possible representation.js
@@ -3,14 +3,19 @@ let str = "aaabbcccc";
 let result = "";
 let count = 1;
 
+if (str.length === 0) {
+    console.log(result);
+}
+
 for (let i = 0; i < str.length; i++) {
 
     if (str[i] === str[i + 1]) {
         count++;
     } else {
-        result = result + str[i] + count;
+        result += str[i] + count;
         count = 1;
     }
 }
 
 console.log(result);
+
