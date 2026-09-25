bb40084fa6a84132d437682613186c70cc336a66
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-25 11:33:20 +0530

    Check_whether_a_string_contains_only_digits.

--- Check_whether_a_string_contains_only_digits..js
+++ Check_whether_a_string_contains_only_digits..js
@@ -1,24 +1,46 @@
-// // Check whether a string contains only digits.
+// // // Check whether a string contains only digits.
+
+// import { myCharCodeAt } from "./CharCodeAt.js";
+
+
+// let str = "uiui7";
+
+// // for(let i=0;i<str.length;i++){
+// //     if(str.charCodeAt(i) < 48 || str.charCodeAt(i) >  57){
+// //         console.log("false");
+// //     }
+// // }
+
+// function check(str){
+//     for(let i=0;i<str.length;i++){
+//     // if(str.charCodeAt(i) < 48 || str.charCodeAt(i) >  57){
+//     if(myCharCodeAt(str , str[i]) < 48 || myCharCodeAt(str , str[i]) >  57 ){
+//         return false;
+//     }
+// }
+// return true;
+// }
+
+// console.log(check("8787"));
+
+
 
 import { myCharCodeAt } from "./CharCodeAt.js";
 
 
-let str = "uiui7";
 
-// for(let i=0;i<str.length;i++){
-//     if(str.charCodeAt(i) < 48 || str.charCodeAt(i) >  57){
-//         console.log("false");
-//     }
-// }
+function check(str) {
 
-function check(str){
-    for(let i=0;i<str.length;i++){
-    // if(str.charCodeAt(i) < 48 || str.charCodeAt(i) >  57){
-    if(myCharCodeAt(str , str[i]) < 48 || myCharCodeAt(str , str[i]) >  57 ){
-        return false;
+    for (let i = 0; i < str.length; i++) {
+
+        let code = myCharCodeAt(str, i);
+
+        if (code < 48 || code > 57) {
+            return false;
+        }
     }
-}
-return true;
+
+    return true;
 }
 
-console.log(check("8787"));
+console.log(check("878962768767"));   
