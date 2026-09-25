054b3a147c4d52257b36838ca0fa24c9c618bc10
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-25 11:18:58 +0530

    Capitalize_the_first_letter

--- Capitalize_the_first_letter.js
+++ Capitalize_the_first_letter.js
@@ -1,20 +1,17 @@
-import { myUpperCase } from "./uppercase.js";
-let str = "hello world javascript";
 
+
+
+import { myUpperCase } from "./uppercase.js";
+
+let str = "hello world javascript";
 
 let ans = "";
 
-let u = myUpperCase(str[0]);
-ans = ans + u;
+for (let i = 0; i < str.length; i++) {
 
-for(let i=1;i<str.length;i++){
-    
-    if(str[i] == " "){
-        ans = ans + " ";
-        // ans = ans + str[i+1].toUpperCase();
-        ans = ans + myUpperCase(str[i+1]);
-        i++;
-    }else{
+    if (i === 0 || str[i - 1] === " ") {
+        ans = ans + myUpperCase(str[i]);
+    } else {
         ans = ans + str[i];
     }
 }
