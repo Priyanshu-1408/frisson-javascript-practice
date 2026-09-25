8ca1bc96b3b6aa70470ede9e19f2c101302e6e8d
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-09-25 15:50:00 +0530

    Find the maximum sum of a contiguous subarray

--- Find the maximum sum of a contiguous subarray.js
+++ Find the maximum sum of a contiguous subarray.js
@@ -1,7 +1,11 @@
-let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 let currentSum = arr[0];
 let maxSum = arr[0];
+
+if (arr.length === 0) {
+    console.log(0);
+}
 
 for (let i = 1; i < arr.length; i++) {
 
